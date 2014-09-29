470e569a899a59819d7a0114c23c2ae7af818489
ne_Sachirou <utakata.c4se@gmail.com>
2014-09-30 02:53:20 +0900

    記事一覧に表示する項目をblog風に。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
   # GET /items
   # GET /items.json
   def index
-    @items = Item.all
+    @items = Item.all.order created_at: :desc
   end
 
   # GET /items/1
